e40335eb91ab6046d6864b2aa569d8a7a41eb6ab
rbouhal <rayanbouhal@gmail.com>
2023-11-06 21:11:26 -0500

    working maze

--- micromouse/maze.js
+++ micromouse/maze.js
@@ -1,150 +1,103 @@
-mazeWidth = 12;
-mazeHeight = 12;
+const mazeContainer = document.getElementById("maze_container");
+const mazeRows = 10; // Change as needed
+const mazeColumns = 10; // Change as needed
+let maze = [];
+
+// Initialize the maze array
+function initializeMaze() {
+    maze = new Array(mazeRows);
+    for (let i = 0; i < mazeRows; i++) {
+        maze[i] = new Array(mazeColumns).fill(0);
+    }
+}
+
+// Create the visual maze structure
 function createBlankMaze() {
-
-    var rowIndex, colIndex;
-
-    var table = document.createElement("table");
-    var tbody = document.createElement("tbody");
-
-    for (rowIndex = 1; rowIndex <= mazeHeight; rowIndex++) {
-
-        var row = document.createElement("tr");
-
-        for (colIndex = 1; colIndex <= mazeWidth; colIndex++) {
-
-            var col = document.createElement("td");
-            if (rowIndex == mazeHeight && colIndex == mazeWidth) {
-                col.setAttribute("type", "start");col.style.display = "flex";
-                col.style.justifyContent = "center";
-                col.style.alignItems = "center";
-                var mouseIcon = document.createElement("img");
-                mouseIcon.setAttribute("src", "icons/mouse.png");
-                mouseIcon.style.width = "35px";
-                mouseIcon.style.height = "35px";
-                // Append the image to the cell
-                col.appendChild(mouseIcon);
-
-            } else if (rowIndex == mazeHeight / 2 && colIndex == mazeWidth / 2) {
-                col.setAttribute("type", "finish");
-                // Create an img element for the finish icon
-                col.style.display = "flex";
-                col.style.justifyContent = "center";
-                col.style.alignItems = "center";
-                var finishIcon = document.createElement("img");
-                finishIcon.setAttribute("src", "icons/finish.png");
-                finishIcon.style.width = "35px";
-                finishIcon.style.height = "35px";
-                // Append the image to the cell
-                col.appendChild(finishIcon);
-
-            }
-            col.setAttribute("id", "cell_" + rowIndex + "_" + colIndex);
-
-            row.appendChild(col);
-
+    initializeMaze();
+    let tableHTML = "";
+    for (let i = 0; i < mazeRows; i++) {
+        let rowHTML = "<tr>";
+        for (let j = 0; j < mazeColumns; j++) {
+            rowHTML += `<td id="cell-${i}-${j}" class="maze-cell"></td>`;
         }
-
-        tbody.appendChild(row);
-
+        rowHTML += "</tr>";
+        tableHTML += rowHTML;
     }
-
-    table.appendChild(tbody);
-
-    document.getElementById("maze_container").appendChild(table);
-
-    var exits = randomPath();
-    let currentRow = mazeHeight; // Start position
-    let currentCol = mazeWidth;
-
-
-    var rowIndex = mazeWidth;
-    var colIndex = mazeHeight;
-
-    for (let exitIndex = 0; exitIndex < exits.length; exitIndex++) {
-        let currentCell = document.getElementById("cell_" + currentRow + "_" + currentCol);
-
-        switch (exits[exitIndex]) {
-            case "left":
-                currentCol = currentCol - 1;
-                currentCell.style.borderLeft = "none";
-                break;
-            case "up":
-                currentRow = currentRow - 1;
-                currentCell.style.borderTop = "none";
-                break;
-            case "right":
-                currentCell.style.borderRight = "none";
-                currentCol = currentCol + 1;
-                break;
-            case "down":
-                currentCell.style.borderBottom = "none";
-                currentRow = currentRow + 1;
-                break;
-        }
-
-        currentCell.style.backgroundColor = "transparent";
-
-        // Remove the opposite border from the adjacent cell
-        let adjacentCell = document.getElementById("cell_" + currentRow + "_" + currentCol);
-        if (adjacentCell) {
-            switch (exits[exitIndex]) {
-                case "left":
-                    adjacentCell.style.borderRight = "none";
-                    break;
-                case "up":
-                    adjacentCell.style.borderBottom = "none";
-                    break;
-                // case "right":
-                //     adjacentCell.style.borderLeft = "none";
-                //     break;
-                // case "down":
-                //     adjacentCell.style.borderTop = "none";
-                //     break;
-            }
-            adjacentCell.style.backgroundColor = "transparent";
-        }
-    }
+    mazeContainer.innerHTML = `<table>${tableHTML}</table>`;
+    placeFinish(); // Place the finish icon at the center
+    generateMazeDFS(); // Start the maze generation
+    placeStart(); // Place the start icon at the bottom right
 }
 
-// Don't revist squares
-function randomPath() {
-    let exits = [];
-    let finishRow = Math.floor(mazeHeight / 2);
-    let finishCol = Math.floor(mazeWidth / 2);
-    let currentRow = mazeHeight; // Start position
-    let currentCol = mazeWidth;
-    while (currentRow > mazeHeight / 2 || currentCol > mazeWidth / 2) {
-        let direction = Math.floor(Math.random() * 4); // Generate a number between 0 and 3
-
-        // Move in a random direction based on the generated number
-        switch (direction) {
-            case 0: // Up
-                if (currentRow > finishRow) {
-                    currentRow--;
-                    exits.push("up");
-                }
-                break;
-            case 1: // Right
-                if (currentCol < mazeWidth - 1) {
-                    currentCol++;
-                    exits.push("right");
-                }
-                break;
-            case 2: // Down
-                if (currentRow < mazeHeight - 1) {
-                    currentRow++;
-                    exits.push("down");
-                }
-                break;
-            case 3: // Left
-                if (currentCol > finishCol) {
-                    currentCol--;
-                    exits.push("left");
-                }
-                break;
-        }
-
-    }
-    return exits;
+// Place the finish icon in the center of the maze
+function placeFinish() {
+    const centerRow = Math.floor(mazeRows / 2);
+    const centerColumn = Math.floor(mazeColumns / 2);
+    const finishCell = document.getElementById(`cell-${centerRow}-${centerColumn}`);
+    finishCell.innerHTML = '<img src="icons/finish.png" alt="Finish" width="35px">';
 }
+
+// Place the start icon at the bottom right of the maze
+function placeStart() {
+    const startCell = document.getElementById(`cell-${mazeRows - 1}-${mazeColumns - 1}`);
+    startCell.innerHTML = '<img src="icons/mouse.png" alt="Start" width="35px">';
+}
+
+// Implementing DFS algorithm to carve out the maze
+function generateMazeDFS() {
+    // Direction vectors
+    const dx = [0, 1, 0, -1];
+    const dy = [-1, 0, 1, 0];
+
+    // Utility function to shuffle an array in-place
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    // Checks if the cell is within the bounds of the maze
+    function isInBounds(x, y) {
+        return x >= 0 && x < mazeColumns && y >= 0 && y < mazeRows;
+    }
+
+    // The recursive DFS function
+    function carvePath(x, y) {
+        // Mark the current cell as visited
+        maze[y][x] = 1;
+        let directions = [0, 1, 2, 3];
+        shuffle(directions); // Shuffle directions to ensure randomness
+
+        // Explore neighbors
+        for (let i = 0; i < directions.length; i++) {
+            let nextX = x + dx[directions[i]];
+            let nextY = y + dy[directions[i]];
+
+            // Check if the neighbor is within bounds and not visited
+            if (isInBounds(nextX, nextY) && maze[nextY][nextX] === 0) {
+                // Carve a path between the current cell and the neighbor
+                if (directions[i] === 1) { // Right
+                    document.getElementById(`cell-${y}-${x}`).style.borderRight = "none";
+                    document.getElementById(`cell-${nextY}-${nextX}`).style.borderLeft = "none";
+                } else if (directions[i] === 3) { // Left
+                    document.getElementById(`cell-${y}-${x}`).style.borderLeft = "none";
+                    document.getElementById(`cell-${nextY}-${nextX}`).style.borderRight = "none";
+                } else if (directions[i] === 0) { // Up
+                    document.getElementById(`cell-${y}-${x}`).style.borderTop = "none";
+                    document.getElementById(`cell-${nextY}-${nextX}`).style.borderBottom = "none";
+                } else if (directions[i] === 2) { // Down
+                    document.getElementById(`cell-${y}-${x}`).style.borderBottom = "none";
+                    document.getElementById(`cell-${nextY}-${nextX}`).style.borderTop = "none";
+                }
+
+                carvePath(nextX, nextY); // Recursive call
+            }
+        }
+    }
+
+    carvePath(0, 0); // Start carving from the top-left corner
+}
+
+
+createBlankMaze();
